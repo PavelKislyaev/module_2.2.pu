263e37f2d0ed920da814a42bd48353cc8fe442b2
PavelKislyaev <pavel.kislyayev@mail.ru>
2024-09-09 09:07:13 +0300

    Add files via upload

---
+++ mdule_2_2.py
@@ -0,0 +1,11 @@
+first = (input("Введите число: "))
+second = input("Введите число: ")
+third = input("Введите число: ")
+if first == first and second and third == first and second and third:
+    print('3')
+if second ==  first and third != first and second and third:
+    print('0')
+elif third ==  second:
+    print('2')
+else:
+    print(third)
